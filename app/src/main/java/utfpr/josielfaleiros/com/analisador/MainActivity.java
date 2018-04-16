6038e02ef38a193406f5ef333d67858f3174900a
JosielFaleiros <josiel.2015@alunos.utfpr.edu.br>
2018-04-16 16:29:52 -0300

    Clear fields working #1

--- app/src/main/java/utfpr/josielfaleiros/com/analisador/MainActivity.java
+++ app/src/main/java/utfpr/josielfaleiros/com/analisador/MainActivity.java
@@ -2,12 +2,24 @@ package utfpr.josielfaleiros.com.analisador;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.RadioGroup;
 
 public class MainActivity extends AppCompatActivity {
-
+    private EditText inputName;
+    private RadioGroup statusGroup;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        inputName = findViewById(R.id.etName);
+        statusGroup = findViewById(R.id.radioGroup);
+    }
+
+    public void cleanData (View view) {
+        inputName.setText("");
+        statusGroup.clearCheck();
     }
 }
